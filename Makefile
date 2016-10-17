cb96236c8d430daf9a94a958a8d149d780c5d532
Michael Hudson-Doyle <michael.hudson@canonical.com>
2016-10-17 21:52:42 +1300

    basic functionality

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # Build-Depends: libnl-route-3-dev
 
-netwatch: netwatch.o
-	gcc -g -O2 -o netwatch netwatch.o `pkg-config --libs libnl-route-3.0`
+netwatch: netwatch.o Makefile
+	gcc -g -O2 -o netwatch netwatch.o  -Wl,--no-as-needed `pkg-config --libs libnl-route-3.0` -Wl,--as-needed
 
 netwatch.o: netwatch.c
 	gcc -g -O2 -o netwatch.o -c netwatch.c `pkg-config --cflags libnl-route-3.0`
--- netwatch.c
+++ netwatch.c
@@ -1,28 +1,89 @@
 #include <netlink/cache.h>
+#include <netlink/route/addr.h>
+#include <netlink/route/link.h>
 
+static void dump_link_info(int act, struct rtnl_link *link) {
+  printf("act: %d ifindex: %d ifname: %s\n", act, rtnl_link_get_ifindex(link),
+         rtnl_link_get_name(link));
+}
 
-static void cb(struct nl_cache * cache, struct nl_object *ob, int dunno, void *data) {
-	printf("cb called with dunno %d\n", dunno);
+static void cb_link(struct nl_cache *cache, struct nl_object *ob, int act,
+                    void *data) {
+  dump_link_info(act, (struct rtnl_link *)ob);
+}
+
+static void e_link(struct nl_object *ob, void *data) {
+  dump_link_info(NL_ACT_NEW, (struct rtnl_link *)ob);
+}
+
+static char buf[100];
+
+static void dump_addr_info(int act, struct rtnl_addr *addr) {
+  struct nl_addr *local = rtnl_addr_get_local(addr);
+  printf("act: %d ifindex: %d local: %s\n", act, rtnl_addr_get_ifindex(addr),
+         nl_addr2str(local, buf, sizeof(buf)));
+}
+
+static void cb_addr(struct nl_cache *cache, struct nl_object *ob, int act,
+                    void *data) {
+  dump_addr_info(act, (struct rtnl_addr *)ob);
+}
+
+static void e_addr(struct nl_object *ob, void *data) {
+  dump_addr_info(NL_ACT_NEW, (struct rtnl_addr *)ob);
 }
 
 int main(int argc, char **argv) {
+  struct nl_sock *sock;
   struct nl_cache_mngr *mngr;
-  struct nl_cache *cache;
+  struct nl_cache *link_cache;
+  struct nl_cache *addr_cache;
   int r;
 
-  // Allocate a new cache manager for RTNETLINK and automatically
-
-  // provide the caches added to the manager.
-
-  r = nl_cache_mngr_alloc(NULL, NETLINK_ROUTE, NL_AUTO_PROVIDE, &mngr);
-  if (r < 0) {
-	  fprintf(stderr, "nl_cache_mngr_alloc failed %d\n", r);
-	  exit(1);
+  sock = nl_socket_alloc();
+  if (sock == NULL) {
+    fprintf(stderr, "nl_socket_alloc failed\n");
+    exit(1);
   }
 
-  r = nl_cache_mngr_add(mngr, "route/link", cb, NULL, &cache);
+  r = nl_cache_mngr_alloc(sock, NETLINK_ROUTE, NL_AUTO_PROVIDE, &mngr);
   if (r < 0) {
-	  fprintf(stderr, "nl_cache_mngr_add failed %d\n", r);
-	  exit(1);
+    fprintf(stderr, "nl_cache_mngr_alloc failed %d\n", r);
+    exit(1);
+  }
+
+  r = rtnl_link_alloc_cache(sock, AF_UNSPEC, &link_cache);
+  if (r < 0) {
+    fprintf(stderr, "rtnl_link_alloc_cache_add failed %d\n", r);
+    exit(1);
+  }
+
+  r = nl_cache_mngr_add_cache(mngr, link_cache, cb_link, NULL);
+  if (r < 0) {
+    fprintf(stderr, "nl_cache_mngr_add_cache failed %d\n", r);
+    exit(1);
+  }
+
+  r = rtnl_addr_alloc_cache(sock, &addr_cache);
+  if (r < 0) {
+    fprintf(stderr, "rtnl_addr_alloc_cache_add failed %d\n", r);
+    exit(1);
+  }
+
+  r = nl_cache_mngr_add_cache(mngr, addr_cache, cb_addr, NULL);
+  if (r < 0) {
+    fprintf(stderr, "nl_cache_mngr_add_cache failed %d\n", r);
+    exit(1);
+  }
+
+  nl_cache_foreach(link_cache, e_link, NULL);
+  nl_cache_foreach(addr_cache, e_addr, NULL);
+
+  while (1) {
+    r = nl_cache_mngr_poll(mngr, 100000);
+    if (r < 0) {
+      fprintf(stderr, "nl_cache_mngr_poll failed %d\n", r);
+      exit(1);
+    }
   }
 }
